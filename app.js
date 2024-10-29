09a055fc6030381a26cee8d5a5e52631add52a5a
Joaquín <magnanimoysimple@gmail.com>
2024-10-29 19:02:55 -0300

    Fixes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const usuarioSchema = mongoose.Schema(
   {
     Nombre: String,
     Edad: Number,
+    Descripcion: String,
     FechaCreacion: Date,
   },
   { versionKey: false }
@@ -24,44 +25,80 @@ const UsuariosModel = mongoose.model("usuarios", usuarioSchema);
 
 // Endpoint para traer todos los usuarios
 app.get("/usuarios", async (req, res) => {
-  const usuarios = await UsuariosModel.find();
-  res.json(usuarios);
+  try {
+    const usuarios = await UsuariosModel.find();
+
+    if (usuarios.length === 0) {
+      return res.status(404).json({ message: "No se encontraron usuarios" });
+    } else {
+      res.json(usuarios);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al leer usuarios", error: error.message });
+  }
 });
 
 // Endpoint para crear un nuevo usuario
 app.post("/usuarios", async (req, res) => {
-  const usuario = new UsuariosModel(req.body);
-  usuario.FechaCreacion = moment().format("D-MM-YYYY");
-  const nuevoUsuario = new UsuariosModel(usuario);
-  const resGuardado = await nuevoUsuario.save();
-  res.status(201).json(resGuardado);
+  try {
+    if (!req.body.Nombre) {
+      return res.status(400).json({ message: "Nombre obligatorio" });
+    }
+
+    const usuario = new UsuariosModel(req.body);
+
+    usuario.FechaCreacion = Date.now();
+
+    const nuevoUsuario = new UsuariosModel(usuario);
+    const resGuardado = await nuevoUsuario.save();
+    res.status(201).json(resGuardado);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al crear el usuario", error: error.message });
+  }
 });
 
 // Endpoint para actualizar un usuario por nombre
 app.put("/usuarios/:nombre", async (req, res) => {
-  const { nombre } = req.params;
-  const nuevoDatos = req.body;
-  const usuarioActualizado = await UsuariosModel.findOneAndUpdate(
-    { Nombre: nombre },
-    nuevoDatos,
-    {
-      new: true,
+  try {
+    const { nombre } = req.params;
+    const nuevoDatos = req.body;
+    const usuarioActualizado = await UsuariosModel.findOneAndUpdate(
+      { Nombre: nombre },
+      nuevoDatos,
+      {
+        new: true,
+      }
+    );
+    if (!usuarioActualizado) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-  );
-  if (!usuarioActualizado) {
-    return res.status(404).json({ message: "Usuario no encontrado" });
+    res.json(usuarioActualizado);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al actualizar el usuario",
+      error: error.message,
+    });
   }
-  res.json(usuarioActualizado);
 });
 
 // Endpoint para obtener un usuario por nombre
 app.get("/usuarios/nombre/:nombre", async (req, res) => {
-  const { nombre } = req.params;
-  const usuario = await UsuariosModel.findOne({ Nombre: nombre });
-  if (!usuario) {
-    return res.status(404).json({ message: "Usuario no encontrado" });
+  try {
+    const { nombre } = req.params;
+    const usuario = await UsuariosModel.findOne({ Nombre: nombre });
+    if (!usuario) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+    res.json(usuario);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al obtener el usuario", error: error.message });
   }
-  res.json(usuario);
 });
 
 // Endpoint para eliminar un usuario por nombre
